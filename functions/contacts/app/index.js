523c87df18b2055ac9c11cfa40fd030e01afe655
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2019-08-26 17:44:18 +1200

    Serve a static list of users

---
+++ functions/contacts/app/index.js
@@ -0,0 +1,59 @@
+/* Express App */
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+
+const data = require('./data');
+
+/* My express App */
+module.exports = function expressApp(functionName) {
+  const app = express();
+  const router = express.Router();
+
+  // gzip responses
+  router.use(compression());
+
+  // Set router base path for local dev
+  const routerBasePath =
+    process.env.NODE_ENV === 'dev'
+      ? `/${functionName}`
+      : `/.netlify/functions/${functionName}/`;
+
+  /* define routes */
+  router.get('/', (req, res) => {
+    res.json(data.users);
+  });
+
+  // Attach logger
+  app.use(morgan(customLogger));
+
+  // Setup routes
+  app.use(routerBasePath, router);
+
+  // Apply express middlewares
+  router.use(cors());
+  router.use(bodyParser.json());
+  router.use(bodyParser.urlencoded({ extended: true }));
+
+  return app;
+};
+
+function customLogger(tokens, req, res) {
+  const log = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'),
+    '-',
+    tokens['response-time'](req, res),
+    'ms',
+  ].join(' ');
+
+  if (process.env.NODE_ENV !== 'dev') {
+    // Log only in AWS context to get back function logs
+    console.log(log);
+  }
+  return log;
+}
